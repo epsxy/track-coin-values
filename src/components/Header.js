f47975f70f7fc405108b3f8473a574be4e7e79b2
Camille <7492974+epsxy@users.noreply.github.com>
2019-03-10 19:08:22 +0100

    add icons for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,72 @@
 import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
 
-const MainTitle = styled(Typography)`
+const HeaderContainer = styled.div`
+  margin-bottom: 65px;
+`;
+
+const AppHeader = styled(AppBar)`
   && {
-    text-align: center;
-    font-size: 4rem;
+    top: 0;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   }
 `;
 
-class Filters extends Component {
+const AppTitleContainer = styled.div`
+  margin-left: 1em;
+  display: flex;
+  align-items: center;
+`;
+
+const AppTitleText = styled(Typography)`
+  && {
+    margin-left: 0.5em;
+  }
+`;
+
+const ContributeContainer = styled.div`
+  margin-right: 1em;
+`;
+
+const GithubProjectLink = styled.a`
+  font-size: 1.5em;
+  color: inherit;
+  &:hover {
+    color: #f50057;
+  }
+`;
+
+class Header extends Component {
   render() {
     return (
-      <div>
-        <MainTitle variant="h1" gutterBottom>
-          Coin tracker
-        </MainTitle>
-      </div>
+      <HeaderContainer>
+        <AppHeader position="absolute" color="primary">
+          <AppTitleContainer>
+            <FontAwesomeIcon icon={faCoins} />
+            <AppTitleText variant="h6" color="inherit">
+              Crypto currency value traker
+            </AppTitleText>
+          </AppTitleContainer>
+          <ContributeContainer>
+            <Typography variant="h6" color="inherit">
+              <GithubProjectLink href="https://github.com/epsxy/track-coin-values">
+                <FontAwesomeIcon icon={faGithub} />
+              </GithubProjectLink>
+            </Typography>
+          </ContributeContainer>
+        </AppHeader>
+      </HeaderContainer>
     );
   }
 }
 
-export default Filters;
+export default Header;
